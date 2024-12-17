3177b0916fe8f6adcb3da1d55da8c8c80e485e55
Rajdip Pramanik <rajdippramanik31@gmail.com>
2024-12-17 11:35:42 +0530

    made the login and register section look awesome

--- CLIENT/src/Screens/Authentication/Login.jsx
+++ CLIENT/src/Screens/Authentication/Login.jsx
@@ -1,28 +1,31 @@
 import React from 'react'
-import Layout from '../../Layouts/Layout'
 import './Register.css'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
     return (
-        <Layout>
-            <form className="form m-auto my-8">
-                <p className="title">Login </p>
-                <p className="message">Signin now and get full access to our app. </p>
-                <label>
-                    <input className="input" type="email" placeholder="" required="" />
-                    <span>Email</span>
-                </label>
+        <>
+            <div className='bg-main w-full' style={{
+                height: '100vh',
+            }}>
+                <form className="form m-auto">
+                    <p className="title">Login </p>
+                    <p className="message">Signin now and get full access to our app. </p>
+                    <label>
+                        <input className="input" type="email" placeholder="" required="" />
+                        <span>Email</span>
+                    </label>
 
-                <label>
-                    <input className="input" type="password" placeholder="" required="" />
-                    <span>Password</span>
-                </label>
-                
-                <button className="submit">Submit</button>
-                <p className="signin">Don't have an acount ? <Link to={`/register`}>Register</Link></p>
-            </form>
-        </Layout>
+                    <label>
+                        <input className="input" type="password" placeholder="" required="" />
+                        <span>Password</span>
+                    </label>
+
+                    <button className="submit">Submit</button>
+                    <p className="signin">Don't have an acount ? <Link to={`/register`}>Register</Link></p>
+                </form>
+            </div>
+        </>
     )
 }
 
--- CLIENT/src/Screens/Authentication/Register.jsx
+++ CLIENT/src/Screens/Authentication/Register.jsx
@@ -1,36 +1,40 @@
 import React from 'react'
-import Layout from '../../Layouts/Layout'
 import './Register.css'
 import { Link } from 'react-router-dom'
 
 const Register = () => {
     return (
-        <Layout>
-            <form className="form m-auto">
-                <p className="title">Register </p>
-                <p className="message">Signup now and get full access to our app. </p>
-                
-                <label>
-                        <input className="input" type="text" placeholder="" required="" />
-                        <span>Firstname</span>
-                    </label>
-                <label>
-                    <input className="input" type="email" placeholder="" required="" />
-                    <span>Email</span>
-                </label>
+        <>
+            <div className='bg-main w-full' style={{
+                height: '100vh',
 
-                <label>
-                    <input className="input" type="password" placeholder="" required="" />
-                    <span>Password</span>
-                </label>
-                <label>
-                    <input className="input" type="password" placeholder="" required="" />
-                    <span>Confirm password</span>
-                </label>
-                <button className="submit">Submit</button>
-                <p className="signin">Already have an acount ? <Link to={`/login`}>Log In</Link></p>
-            </form>
-        </Layout>
+            }}>
+                <form className="form mx-auto">
+                    <p className="title">Register </p>
+                    <p className="message">Signup now and get full access to our app. </p>
+
+                    <label>
+                        <input className="input" type="text" placeholder="" required="" />
+                        <span>Full Name</span>
+                    </label>
+                    <label>
+                        <input className="input" type="email" placeholder="" required="" />
+                        <span>Email</span>
+                    </label>
+
+                    <label>
+                        <input className="input" type="password" placeholder="" required="" />
+                        <span>Password</span>
+                    </label>
+                    <label>
+                        <input className="input" type="password" placeholder="" required="" />
+                        <span>Confirm password</span>
+                    </label>
+                    <button className="submit">Submit</button>
+                    <p className="signin">Already have an acount ? <Link to={`/login`}>Log In</Link></p>
+                </form>
+            </div>
+        </>
     )
 }
 
